50b4800cc4c1dbe4c83cadc2086e273b14b6f155
Jatin Iyer <jatin096@gmail.com>
2022-04-25 12:57:59 +0530

    fixes for ec2

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
     dialect: 'mysql',
-    host: 'jatin.chzjgzr6vsty.ap-south-1.rds.amazonaws.com',
+    // host: 'jatin.chzjgzr6vsty.ap-south-1.rds.amazonaws.com',
     define: {
         freezeTableName: true,
         timestamps: false
